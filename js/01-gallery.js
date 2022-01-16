13adc495a3d251007ec4dea2f88fcb30e32e8bb8
ololosh-tralevich <karpeko76@gmail.com>
2022-01-16 20:39:56 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let galleryEl = document.querySelector('.gallery');
+
+const listPicsEl = galleryItems
+  .map(
+    pic => `<div class="gallery__item">
+                <a class="gallery__link" href=${pic.original}>
+                    <img
+                    class="gallery__image"
+                    src=${pic.preview}
+                    data-source=${pic.original}
+                    alt=${pic.description}
+                    />
+                </a>
+            </div>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', listPicsEl);
+
+for (let i = 0; i < document.getElementsByClassName('gallery__item').length; i++) {
+  document.getElementsByClassName('gallery__link')[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    return false;
+  });
+  const currentImg = document.getElementsByClassName('gallery__image')[i].dataset.source;
+  document.getElementsByClassName('gallery__item')[i].onclick = () => {
+    const instance = basicLightbox.create(`<img width="1280" height="720" src=${currentImg}>`);
+    instance.show();
+    document.addEventListener('keyup', e => {
+      if (e.code === 'Escape') instance.close();
+    });
+  };
+}
