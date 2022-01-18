361f760bf29a26000e76ef51e68c081935e1b9cb
ololosh-tralevich <karpeko76@gmail.com>
2022-01-18 10:01:16 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
 
-let galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector(".gallery");
+const galleryItemEl = document.getElementsByClassName("gallery__item");
+const galleryLinkEl = document.getElementsByClassName("gallery__link");
+const galleryPicEl = document.getElementsByClassName("gallery__image");
 
 const listPicsEl = galleryItems
   .map(
     (pic) => `<div class="gallery__item">
-                <a class="gallery__link" href=${pic.original}>
+                <a class="gallery__link" href="${pic.original}">
                     <img
                     class="gallery__image"
-                    src=${pic.preview}
-                    data-source=${pic.original}
-                    alt=${pic.description}
+                    src="${pic.preview}"
+                    data-source="${pic.original}"
+                    alt="${pic.description}"
                     />
                 </a>
             </div>`
@@ -19,25 +22,18 @@ const listPicsEl = galleryItems
 
 galleryEl.insertAdjacentHTML("beforeend", listPicsEl);
 
-for (
-  let i = 0;
-  i < document.getElementsByClassName("gallery__item").length;
-  i++
-) {
-  document
-    .getElementsByClassName("gallery__link")
-    [i].addEventListener("click", function (event) {
-      event.preventDefault();
-      return false;
-    });
-  const currentImg =
-    document.getElementsByClassName("gallery__image")[i].dataset.source;
-  document.getElementsByClassName("gallery__item")[i].onclick = () => {
+for (let i = 0; i < galleryItemEl.length; i++) {
+  galleryLinkEl[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    return false;
+  });
+  const currentImg = galleryPicEl[i].dataset.source;
+  galleryItemEl[i].onclick = () => {
     const instance = basicLightbox.create(
-      `<img width="1280" height="720" src=${currentImg}>`
+      `<img width="1280" height="720" src="${currentImg}">`
     );
     instance.show();
-    document.addEventListener("keyup", (e) => {
+    document.addEventListener("keydown", (e) => {
       if (e.code === "Escape") instance.close();
     });
   };
